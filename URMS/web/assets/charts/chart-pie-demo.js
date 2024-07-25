7ea28735c930a0e2bdd21a07baca2bbaa5edb63d
FoibeU <f.uwizeyima@alustudent.com>
2024-07-25 13:32:19 +0200

    continue developing

--- URMS/web/assets/charts/chart-pie-demo.js
+++ URMS/web/assets/charts/chart-pie-demo.js
@@ -1,9 +1,4 @@
-/*!
-    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2023 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
+
 // Scripts
 // 
 
@@ -12,10 +7,10 @@ window.addEventListener('DOMContentLoaded', event => {
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
+    
+         if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+         document.body.classList.toggle('sb-sidenav-toggled');
+         }
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
@@ -75,7 +70,6 @@ const mockMaintenanceData = {
 
 // Function to create and update charts
 function createCharts() {
-    // Area Chart (Occupancy Rate)
     var ctxArea = document.getElementById('myAreaChart').getContext('2d');
     var myAreaChart = new Chart(ctxArea, {
         type: 'doughnut', // Can change to 'bar', 'line', etc. as needed
